2bb1595c285810c699604de96a7afb43428c5b95
my <849320908@qq.com>
2019-06-19 14:25:38 +0800

    第一次：创建项目

---
+++ src/main/java/com/my/demo1/Test.java
@@ -0,0 +1,19 @@
+package com.my.demo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+
+    //@GetMapping("/hello")
+    @RequestMapping(value = "/hello" , method = RequestMethod.GET)
+
+    public String a(){
+
+        return "aaa";
+    }
+}
